1c11a131aabf61ec00b842d9eaf63e95d755a2a4
Zonghuan Li <Z.Li-25@tudelft.nl>
2024-03-19 15:19:26 +0100

    add badge_bleak

--- BadgeFramework/bleak_test.py
+++ BadgeFramework/bleak_test.py
@@ -2,7 +2,6 @@ import asyncio
 import queue
 
 import pandas
-import pandas as pd
 from bleak import BleakClient, BleakScanner, BleakGATTCharacteristic
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
@@ -11,6 +10,8 @@ import struct
 import badge
 from badge_protocol import Request
 import badge_bleak
+from hub_connection_bleak import Connection
+from badge import OpenBadge
 
 
 def get_mac_address(df, badge_id: int) -> str:
@@ -22,6 +23,7 @@ def get_mac_address(df, badge_id: int) -> str:
 UART_SERVICE_UUID = uuid.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 TX_CHAR_UUID = uuid.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
 RX_CHAR_UUID = uuid.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
+RX_CHAR_UUID_2 = uuid.UUID("00002902-0000-1000-8000-00805F9b34FB")
 
 
 def is_spcl_midge(device: BLEDevice) -> bool:
@@ -38,8 +40,13 @@ def get_device_id(device: BLEDevice, addr_df: pandas.DataFrame) -> int:
     return badge_id
 
 
+def callback(sender: BleakGATTCharacteristic, data: bytearray):
+    if len(data) > 0:
+        print(f"RX changed {sender}: {data}")
+
+
 async def main():
-    df_badge = pd.read_csv("mappings_allBackup.csv")
+    df_badge = pandas.read_csv("mappings_allBackup.csv")
     # Find all devices
     # using both BLEdevice and advertisement data here since it has more information. Also mutes the warning.
     # devices = await badge_scanner.discover(timeout=2.0)
@@ -58,7 +65,7 @@ async def main():
         # Connect to the midge
         async with BleakClient(ble_device.address) as client:
             serv = client.services
-
+            # char_13 = serv.char_13[-1]
             uart = serv.get_service(UART_SERVICE_UUID)
             # self.uart = self.conn.getServiceByUUID(UART_SERVICE_UUID) # Bluepy equivalent
 
@@ -67,36 +74,30 @@ async def main():
 
             tx = serv.get_characteristic(TX_CHAR_UUID)
             # self.tx = self.uart.getCharacteristics(TX_CHAR_UUID)[0] # Bluepy equivalent
+            # my_badge = OpenBadge(client)
+            message = b'\x01\x00\x1e'
+            # val = struct.pack("<bb", 0x01, 0x00)
+            # a = my_badge.get_free_sdc_space()
+            # await client.start_notify(11, callback)
+            await client.start_notify(rx, callback)
+            # await client.write_gatt_char(0x0013, val, response=False)
+            await client.write_gatt_char(TX_CHAR_UUID, message, response=True)
+            # response_11 = await client.read_gatt_char(11)
 
-            # Trying to replicate this bluepy code with bleak
+            response_rx = b''
+            while client.is_connected:
+                if len(response_rx) > 0:
+                    break
+                response_rx = await client.read_gatt_char(RX_CHAR_UUID)
+            # handle: 18, data: b'\x10\x00\x05\xbd\x0e\x00\x00\xb7\x0e\x00\x00\x0e\x00\x00\x00\xd2\x01\x00'
 
-            # # Turn on notification of RX characteristics
-            # logger.debug("Subscribing to RX characteristic changes...")
-            # CONFIG_HANDLE = 0x0013
-            # # was wrong 0x000c
-            # self.conn.writeCharacteristic(
-            #     handle=CONFIG_HANDLE, val=struct.pack("<bb", 0x01, 0x00)
-            # )
-            # self.conn.setDelegate(SimpleDelegate(bleconn=self))
-
-            #
-            # Option 1, not sure how to create a new BleakGATTCharacteristic
-            #
-            # noti = BleakGATTCharacteristic()
-            # serv.add_characteristic(noti)
-
-            #
-            # Option 2, does not work because the CONFIG_HANDLE is not a recognised
-            #
-            CONFIG_HANDLE = 0x0013
-            ACTIVATION_VAL = struct.pack("<bb", 0x01, 0x00)
-            # await client.write_gatt_char(CONFIG_HANDLE, ACTIVATION_VAL)
-            await client.write_gatt_char(RX_CHAR_UUID, ACTIVATION_VAL, response=True)
-
-            bleak_badge = badge_bleak.OpenBadge(client)
-            bleak_badge.get_status()
             c = 9
-            #
+
+
+            # bleak_badge = badge_bleak.OpenBadge(client)
+            # bleak_badge.get_status()
+            # c = 9
+            # #
             # Option 3, it doesn't crash but not sure if it actually works
             #
             # def callback(sender: BleakGATTCharacteristic, data: bytearray):
